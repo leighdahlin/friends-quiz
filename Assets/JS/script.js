586dac5dffaa820b63bdffc11fc1328f14a627a6
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-04-13 12:20:03 -0700

    Changed how js will populate questions, made hidden form that will appear when start button is clicked. Array will now run function to randomize order when the page loads.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,20 +1,23 @@
-var secondsLeft = 5;
+var secondsLeft = 0;
 var timeEl = document.querySelector("#time");
 var startButtonEl = document.querySelector(".start-button");
-var headingEl = document.querySelector(".main-content-head");
+var headingEl = document.querySelector("#main-content-head");
 var subContentEl = document.querySelector(".sub-content");
 var mainContentEl = document.querySelector(".main-content");
+var questionEl = document.querySelector("#question-head");
+var button1El = document.querySelector("#button1");
+var button2El = document.querySelector("#button2");
+var button3El = document.querySelector("#button3");
+var button4El = document.querySelector("#button4");
 
-// var objectLength = Object.keys(questionBank).length;
-// var question1 = ["What is an object in Javascript?", "A correlation of properties", "An array but bigger", "A function for strings", "A lot of things"];
-// var question2 = ["Which best describes the window object?", "A", "B", "C", "D"];
 var question1 = {
     question: "What is an object in Javascript?",
     choice1: "A correlation of properties",
     choice2: "An array but bigger",
     choice3: "A function for strings",
-    choice4: "A function for strings"
-}
+    choice4: "A function for strings",
+    answer: this.choice1, //see if I could use this to check answers
+};
 
 var question2 = {
     question: "Which best describes the window object?",
@@ -22,7 +25,7 @@ var question2 = {
     choice2: "B",
     choice3: "C",
     choice4: "D"
-}
+};
 
 var question3 = {
     question: "How do you start a function?",
@@ -30,7 +33,7 @@ var question3 = {
     choice2: "F",
     choice3: "G",
     choice4: "H"
-}
+};
 
 var question4 = {
     question: "What is an event listener?",
@@ -38,7 +41,7 @@ var question4 = {
     choice2: "J",
     choice3: "K",
     choice4: "L"
-}
+};
 
 var question5 = {
     question: "Which of the timer intervals below will execute?",
@@ -46,157 +49,184 @@ var question5 = {
     choice2: "N",
     choice3: "O",
     choice4: "P"
-}
+};
 
-var answerKey = {
-    question1: "choice1",
-    question2: "choice2"
-}
 
 var questionBank = [question1, question2, question3, question4, question5];
+var currentQuestionIndex = 0;
+var currentQuestion = questionBank[currentQuestionIndex].question;
+var currentChoice1 = questionBank[currentQuestionIndex].choice1;
+var currentChoice2 = questionBank[currentQuestionIndex].choice2;
+var currentChoice3 = questionBank[currentQuestionIndex].choice3;
+var currentChoice4 = questionBank[currentQuestionIndex].choice4;
 
 
-
-// var questionBank = {
-//     question1: ["What is an object in Javascript?", "A correlation of properties", "An array but bigger", "A function for strings", "A lot of things"],
-//     question2: ["Which best describes the window object?", "A", "B", "C", "D"],
-//     question3: ["How do you start a function?", "A", "B", "C", "D"],
-//     question4: ["What is an event listener?", "A", "B", "C", "D"],
-//     question5: ["Which of the timer intervals below will execute?", "A", "B", "C", "D"]
-// }
 
 startButtonEl.addEventListener("click", setTime);
-startButtonEl.addEventListener("click", firstQuestionPopUp);
+startButtonEl.addEventListener("click", questionPopUps);
+
+//using the Fisher-Yates shuffle
+function shuffle(array) {
+    for (var i = array.length-1; i > 0; i--){
+        let j = Math.floor(Math.random()*(i+1));
+        [array[i], array[j]] = [array[j], array[i]]
+        array.join('');
+    }
+    console.log(array);
+}
 
 
 function setTime() {
+    secondsLeft = 61;
 var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = "Timer: " + secondsLeft;
+    timeEl.textContent = "Timer: " + secondsLeft ;
     
     if(secondsLeft === 0) {
         clearInterval(timerInterval);
         timesUpMessage();
+       
     }
 }
 , 1000)
 };
 
-// function questionPopUps() {
-//     firstQuestionPopUp();
-//     nextQuestionPopUp();
-// };
-
-function firstQuestionPopUp() {
-// console.log(Object.keys(questionBank).length);
-// console.log(questionBank.question1[0]);
-
-var randomQuestionIndex = questionBank[Math.floor(Math.random()*questionBank.length)];
-var randomQuestion = randomQuestionIndex.question;
-// console.log(randomQuestionIndex);
-// console.log(questionBank[0]);
-// console.log(questionBank[1]);
-// console.log(questionBank[2]);
-// console.log(questionBank[3]);
-// console.log(questionBank[4]);
-
-
-// for(var i = 0; i < questionBank.length; i++) {
+function questionPopUps() {
+    removeIntoPage();
+    QuestionPopUp();
+    for(var i = 1; i < questionBank.length; i++) {
+        currentQuestionIndex = i;
+        QuestionPopUp();
+    }
     
-// }
+};
 
- headingEl.textContent = randomQuestion;
- subContentEl.remove();
- startButtonEl.remove();
+function removeIntoPage() {
+    headingEl.setAttribute("class", "hidden");
+    subContentEl.remove();
+    startButtonEl.remove();
+}
 
+function QuestionPopUp(event) {
+    questionEl.textContent = currentQuestion;
 
-var choice1 = randomQuestionIndex.choice1;
-var choice2 = randomQuestionIndex.choice2;
-var choice3 = randomQuestionIndex.choice3;
-var choice4 = randomQuestionIndex.choice4;
+    button1El.setAttribute("value", currentChoice1);
+    button1El.setAttribute("class", "visible");
 
-var buttonDiv = document.createElement("div");
-buttonDiv.setAttribute("id", "button-div");
-buttonDiv.setAttribute("style", "display: flex; flex-direction: column; width: 45%;");
-mainContentEl.appendChild(buttonDiv);
-var buttonEl = document.querySelector("#button-div");
+    button2El.setAttribute("value", currentChoice2);
+    button2El.setAttribute("class", "visible");
 
-var choiceButton1 = document.createElement("button");
-choiceButton1.setAttribute("id", "choice-button-1");
-choiceButton1.setAttribute("style", "text-align: left;");
-var buttonQuestion = document.createTextNode(choice1);
-choiceButton1.appendChild(buttonQuestion);
-buttonEl.appendChild(choiceButton1);
+    button3El.setAttribute("value", currentChoice3);
+    button3El.setAttribute("class", "visible");
+
+    button4El.setAttribute("value", currentChoice4);
+    button4El.setAttribute("class", "visible");
 
 
-var choiceButton2 = document.createElement("button");
-choiceButton2.setAttribute("id", "choice-button-2");
-choiceButton2.setAttribute("style", "text-align: left;");
-var buttonQuestion = document.createTextNode(choice2);
-choiceButton2.appendChild(buttonQuestion);
-buttonEl.appendChild(choiceButton2);
 
-var choiceButton3 = document.createElement("button");
-choiceButton3.setAttribute("id", "choice-button-3");
-choiceButton3.setAttribute("style", "text-align: left;");
-var buttonQuestion = document.createTextNode(choice3);
-choiceButton3.appendChild(buttonQuestion);
-buttonEl.appendChild(choiceButton3);
+// var buttonDiv = document.createElement("div");
+// buttonDiv.setAttribute("id", "button-div");
+// buttonDiv.setAttribute("style", "display: flex; flex-direction: column; width: 45%;");
+// mainContentEl.appendChild(buttonDiv);
+// var buttonEl = document.querySelector("#button-div");
 
-var choiceButton4 = document.createElement("button");
-choiceButton4.setAttribute("id", "choice-button-4");
-choiceButton4.setAttribute("style", "text-align: left;");
-var buttonQuestion = document.createTextNode(choice4);
-choiceButton4.appendChild(buttonQuestion);
-buttonEl.appendChild(choiceButton4);
+// var choiceButton1 = document.createElement("button");
+// choiceButton1.setAttribute("id", "choice-button-1");
+// choiceButton1.setAttribute("class", "button");
+// choiceButton1.setAttribute("style", "text-align: left;"); //could use querySelectorAll to select all of these and set certain attributes using for loop
+// var buttonQuestion = document.createTextNode(choice1);
+// choiceButton1.appendChild(buttonQuestion);
+// buttonEl.appendChild(choiceButton1);
 
 
-var nextButton = document.createElement("button");
-nextButton.setAttribute("id", "next-button")
-var textNode = document.createTextNode("Next");
-nextButton.appendChild(textNode);
-mainContentEl.appendChild(nextButton);
-var nextButtonEl = document.querySelector("#next-button");
+// var choiceButton2 = document.createElement("button");
+// choiceButton2.setAttribute("id", "choice-button-2");
+// choiceButton2.setAttribute("class", "button");
+// choiceButton2.setAttribute("style", "text-align: left;");
+// var buttonQuestion = document.createTextNode(choice2);
+// choiceButton2.appendChild(buttonQuestion);
+// buttonEl.appendChild(choiceButton2);
 
-nextButtonEl.addEventListener("click", nextQuestionPopUp);
+// var choiceButton3 = document.createElement("button");
+// choiceButton3.setAttribute("id", "choice-button-3");
+// choiceButton3.setAttribute("class", "button");
+// choiceButton3.setAttribute("style", "text-align: left;");
+// var buttonQuestion = document.createTextNode(choice3);
+// choiceButton3.appendChild(buttonQuestion);
+// buttonEl.appendChild(choiceButton3);
+
+// var choiceButton4 = document.createElement("button");
+// choiceButton4.setAttribute("id", "choice-button-4");
+// choiceButton4.setAttribute("class", "button");
+// choiceButton4.setAttribute("style", "text-align: left;");
+// var buttonQuestion = document.createTextNode(choice4);
+// choiceButton4.appendChild(buttonQuestion);
+// buttonEl.appendChild(choiceButton4);
+
+
+
+// var nextButton = document.createElement("button");
+// nextButton.setAttribute("id", "next-button")
+// var textNode = document.createTextNode("Next");
+// nextButton.appendChild(textNode);
+// mainContentEl.appendChild(nextButton);
+// var nextButtonEl = document.querySelector("#next-button");
+
+// nextButtonEl.addEventListener("click", nextQuestionPopUp);
 
 }
 
-function nextQuestionPopUp() {
-    var randomQuestionIndex = questionBank[Math.floor(Math.random()*questionBank.length)];
-    var randomQuestion = randomQuestionIndex.question;
+// function nextQuestionPopUp() {
+//     var randomQuestionIndex = questionBank[Math.floor(Math.random()*questionBank.length)];
+//     var randomQuestion = randomQuestionIndex.question;
 
-    headingEl.textContent = randomQuestion;
-    console.log(randomQuestionIndex.choice1)
+//     headingEl.textContent = randomQuestion;
+//     console.log(randomQuestionIndex.choice1)
 
     // var choice1 = randomQuestionIndex.choice1;
     // var choice2 = randomQuestionIndex.choice2;
     // var choice3 = randomQuestionIndex.choice3;
     // var choice4 = randomQuestionIndex.choice4;
 
-    var choiceButton1El = document.querySelector("#choice-button-1");
-    var choiceButton1E2 = document.querySelector("#choice-button-2");
-    var choiceButton1E3 = document.querySelector("#choice-button-3");
-    var choiceButton1E4 = document.querySelector("#choice-button-4");
+//     var choiceButton1El = document.querySelector("#choice-button-1");
+//     var choiceButton1E2 = document.querySelector("#choice-button-2");
+//     var choiceButton1E3 = document.querySelector("#choice-button-3");
+//     var choiceButton1E4 = document.querySelector("#choice-button-4");
 
-    choiceButton1El.textContent = randomQuestionIndex.choice1;
-    choiceButton1E2.textContent = randomQuestionIndex.choice2;
-    choiceButton1E3.textContent = randomQuestionIndex.choice3;
-    choiceButton1E4.textContent = randomQuestionIndex.choice4;
+//     choiceButton1El.textContent = randomQuestionIndex.choice1;
+//     choiceButton1E2.textContent = randomQuestionIndex.choice2;
+//     choiceButton1E3.textContent = randomQuestionIndex.choice3;
+//     choiceButton1E4.textContent = randomQuestionIndex.choice4;
 
-    var nextButtonEl = document.querySelector("#next-button");
+//     var nextButtonEl = document.querySelector("#next-button");
 
-    nextButtonEl.addEventListener("click", nextQuestionPopUp);
-}
-
-function timesUpMessage() {
-    headingEl.textContent = "Your time is up!";
-    subContentEl.textContent = "";
-
-}
-
-// function init() {
-//     setTime();
+//     nextButtonEl.addEventListener("click", nextQuestionPopUp);
 // }
 
-// init();
+function timesUpMessage() {
+
+    questionEl.remove();
+    button1El.remove();
+    button2El.remove();
+    button3El.remove();
+    button4El.remove();
+    // button1El.setAttribute("value", "");
+    // button1El.setAttribute("class", "hidden");
+    // button2El.setAttribute("value", "");
+    // button2El.setAttribute("class", "hidden");
+    // button3El.setAttribute("value", "");
+    // button3El.setAttribute("class", "hidden");
+    // button4El.setAttribute("value", "");
+    // button4El.setAttribute("class", "hidden");
+
+    headingEl.setAttribute("class", "visible");
+    headingEl.textContent = "Your time is up!";
+
+}
+
+function init() {
+    shuffle(questionBank);
+}
+
+init();
+
